2ae4fc9948718ac23647d4c3bfe50289ee540993
Likhesh Mahajan <likhesh@Mac-Pro.local>
2020-04-22 11:52:49 +1000

    size of image modified

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -6,7 +6,7 @@ const Card = (props) => {
     <div className="card-container">
       <img
         alt="monster"
-        src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
+        src={`https://robohash.org/${props.monster.id}?set=set2&size=180x230`}
       />
       <h2>{props.monster.name}</h2>
       <p>{props.monster.email}</p>
